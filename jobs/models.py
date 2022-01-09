8211a1dff5c542fd3bc2d0148c1d556ae050b7ca
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-01-09 18:52:33 -0300

    Jobs app started and added to Admin

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    """
+    Stores a single job entry, related to :model:`blog.Blog`.
+    """
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000, null=True)
+    image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.title
