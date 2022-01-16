ac874167125d2d1378e39857e589d655e6166e44
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-01-16 11:06:50 -0300

    Shortcut for get project view

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Project
 
 
 def index(request):
-    projects = Project.objects.order_by('-pub_date')
-    return HttpResponse(projects)
+    projects = Project.objects.order_by('priority_order')
+    return render(request, 'projects/index.html', {'projects': projects})
 
 
 def detail(request, project_id):
-    return HttpResponse("Welcome to project with id: %s" % project_id)
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html',{'project': project})
