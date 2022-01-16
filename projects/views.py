ee1fd294238d6a0a13b1e6f66be2c6d73bfddb3d
Geovane Pacheco <geovane.pacheco99@gmail.com>
2022-01-16 15:19:57 -0300

    Generic views added

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,24 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from .models import Project
 
 
-def index(request):
-    projects = Project.objects.order_by('priority_order')
-    return render(request, 'projects/index.html', {'projects': projects})
+class IndexView(generic.ListView):
+    template_name = 'projects/index.html'
+
+    def get_queryset(self):
+        """Returns all projects ordered by priority"""
+        return Project.objects.order_by('-priority_order')
+
+class DetailView(generic.DetailView):
+    model = Project
+    template_name = 'projects/detail.html'
+
+# def index(request):
+#     projects = Project.objects.order_by('priority_order')
+#     return render(request, 'projects/index.html', {'projects': projects})
 
 
-def detail(request, project_id):
-    project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'projects/detail.html',{'project': project})
+# def detail(request, project_id):
+#     project = get_object_or_404(Project, pk=project_id)
+#     return render(request, 'projects/detail.html',{'project': project})
